982b01ad02116f7d575118a865f7bc4e6dc7ee0b
Nikkza <isaksson.nicklas80@outlook.com>
2020-04-29 17:27:02 +0300

    remove list

--- Larrgestfivenumbers/Logichandler.cs
+++ Larrgestfivenumbers/Logichandler.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Largestfivenumbers
 {
@@ -13,7 +10,6 @@ namespace Largestfivenumbers
             int[] numbers = { };
             try
             {
-                List<int> list = new List<int>();
                 var parse = num.ToCharArray();
                 numbers = Array.ConvertAll(parse, c => (int)Char.GetNumericValue(c));
             }
